6e7142968f55c58cf2250a462b0bdc7885fde30b
faykarr <nasyathfaykar@gmail.com>
2023-12-30 20:29:55 +0700

    feat: added initiat Myth/Auth library & setup

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -8,6 +8,9 @@ use CodeIgniter\Filters\DebugToolbar;
 use CodeIgniter\Filters\Honeypot;
 use CodeIgniter\Filters\InvalidChars;
 use CodeIgniter\Filters\SecureHeaders;
+use Myth\Auth\Filters\LoginFilter;
+use Myth\Auth\Filters\RoleFilter;
+use Myth\Auth\Filters\PermissionFilter;
 
 class Filters extends BaseConfig
 {
@@ -24,6 +27,9 @@ class Filters extends BaseConfig
         'honeypot'      => Honeypot::class,
         'invalidchars'  => InvalidChars::class,
         'secureheaders' => SecureHeaders::class,
+        'login'      => LoginFilter::class,
+        'role'       => RoleFilter::class,
+        'permission' => PermissionFilter::class,
     ];
 
     /**
@@ -34,8 +40,9 @@ class Filters extends BaseConfig
      */
     public array $globals = [
         'before' => [
-            // 'honeypot',
-            // 'csrf',
+            'honeypot',
+            'csrf',
+            'login'
             // 'invalidchars',
         ],
         'after' => [
